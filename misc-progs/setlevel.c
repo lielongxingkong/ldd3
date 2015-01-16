72bb94ebc94d7d43399aa1a91974e61c6b09d5eb
Zhao Zhenlong <zhaozhl@inspur.com>
2015-01-16 17:52:30 +0800

    update syscall in userspace calling way for v2.6.32

--- misc-progs/setlevel.c
+++ misc-progs/setlevel.c
@@ -24,10 +24,9 @@
 #include <errno.h>
 /* #include <unistd.h> */ /* conflicting on the alpha */
 #define __LIBRARY__ /* _syscall3 and friends are only available through this */
-#include <linux/unistd.h>
+#include <syscall.h>
 
-/* define the system call, to override the library function */
-_syscall3(int, syslog, int, type, char *, bufp, int, len);
+extern int syscall(int, ...);
 
 int main(int argc, char **argv)
 {
@@ -38,7 +37,7 @@ int main(int argc, char **argv)
     } else {
         fprintf(stderr, "%s: need a single arg\n",argv[0]); exit(1);
     }
-    if (syslog(8,NULL,level) < 0) {  
+    if (syscall(SYS_syslog, 8, NULL, level) < 0) {  
         fprintf(stderr,"%s: syslog(setlevel): %s\n",
                 argv[0],strerror(errno));
         exit(1);
